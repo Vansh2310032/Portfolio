0c6089608340d1f33a60646f6edb5900f9d1ae7a
Vansh2310032 <vansh2310032@akgec.ac.in>
2024-10-12 01:31:44 +0530

    almost done

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".text", {
+        strings: ["UI/UX Designer", "Video Editor", "UE5 Designer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
+
+
